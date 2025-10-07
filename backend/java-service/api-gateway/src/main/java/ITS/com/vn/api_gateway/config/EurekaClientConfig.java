843046e424865bfc58027e0d7782c32adc8179af
ngocthach <huynhngocthach2004@gmail.com>
2025-10-07 23:40:54 +0700

    add api gateway

--- backend/java-service/api-gateway/src/main/java/ITS/com/vn/api_gateway/config/EurekaClientConfig.java
+++ backend/java-service/api-gateway/src/main/java/ITS/com/vn/api_gateway/config/EurekaClientConfig.java
@@ -1,4 +1,20 @@
 package ITS.com.vn.api_gateway.config;
-
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+@Configuration
 public class EurekaClientConfig {
+
+//    @Bean
+//    public RestTemplateBuilder restTemplateBuilder() {
+//        return new RestTemplateBuilder();
+//    }
+//
+//    @Bean
+//    public RestTemplate eurekaRestTemplate(RestTemplateBuilder builder) {
+//        return builder
+//                .basicAuthentication("eureka", "12345678")
+//                .build();
+//    }
 }
